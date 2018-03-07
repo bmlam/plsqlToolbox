d0dcc96a8815ca525262dc1dff6c3b278f76c6cf
Bon-Minh Lam <bmlam@sefrowo.com>
2018-03-07 13:39:16 +0100

    added comments

--- dbms_pipe_example.sql
+++ dbms_pipe_example.sql
@@ -19,11 +19,21 @@ create or replace procedure pipe_sender AS
 begin
       FOR m_ix IN 1 .. 3 LOOP
          dbms_output.put_line( 'Line '||$$plsql_line||': '|| m_ix );
+         -- in real life the normal use case is that there are many senders and one receiver
+         -- when updating the format of the payload messae sent over the pipe, some senders may still be
+         -- using the older formats.
+         -- It is a good idea to exchange the protocol version as the first step.The receiver  
+         -- can cater for old formats based on the version so that things are not broken.
+         dbms_pipe.pack_message( l_protocol_version ); 
          dbms_pipe.pack_message( 1233 + m_ix );
          dbms_pipe.pack_message( rpad( 'how do you do?', 4000,'*' ) ); -- make message real big to force buffer full condition
          FOR try IN 1 .. 3 LOOP
             dbms_output.put_line( 'Line '||$$plsql_line||': '|| try );
             l_rc := dbms_pipe.send_message( '2018test' , timeout => 3 );
+            -- not that buffer full may also result in "ORA-06558: buffer in dbms_pipe package is full.  No more items allowed" 
+            -- it seems that Oracle set a package variable to remember this status. Even if the pipe has been unloaded later on
+            -- for the session, the pipe is unusable. This is proved by running DBMS_SESSION.RESET_PACKAGE but this is usually
+            -- not a viable option. 
             CASE l_rc 
             WHEN 0 THEN 
                EXIT;
@@ -42,6 +52,7 @@ show errors
 
 create or replace procedure pipe_reader AS
    l_rc INTEGER;
+   l_protocol_version INTEGER;
    l_num NUMBER;
    l_text VARCHAR2( 30000 BYTE );
    lc_timeout CONSTANT INTEGER := 2;
@@ -60,6 +71,8 @@ begin
       ELSE 
          RAISE_APPLICATION_ERROR( -20003, 'unexpected RC '||l_rc );
       END CASE;
+      dbms_pipe.unpack_message( l_protocol_version );
+      dbms_output.put_line( 'Line '||$$plsql_line||': '|| l_protocol_version );
       dbms_pipe.unpack_message( l_num );
       dbms_output.put_line( 'Line '||$$plsql_line||': '|| l_num );
       dbms_pipe.unpack_message( l_text );
